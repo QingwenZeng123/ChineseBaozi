a9f37f2c03d1c937860b43cb0b82b9ae3d300780
QingwenZeng123 <97718719+QingwenZeng123@users.noreply.github.com>
2023-04-30 01:25:28 -0700

    Update generator.test.js

--- test/generator.test.js
+++ test/generator.test.js
@@ -19,55 +19,55 @@ const fixtures = [
       console.log(第一);
     `,
   },
-  //   {
-  //     name: "while",
-  //     source: `
-  //       let x = 0;
-  //       while x < 5 {
-  //         let y = 0;
-  //         while y < 5 {
-  //           print x * y;
-  //           y = y + 1;
-  //         }
-  //         x = x + 1;
-  //       }
-  //     `,
-  //     expected: dedent`
-  //       let x_1 = 0;
-  //       while ((x_1 < 5)) {
-  //       let y_2 = 0;
-  //       while ((y_2 < 5)) {
-  //       console.log((x_1 * y_2));
-  //       y_2 = (y_2 + 1);
-  //       }
-  //       x_1 = (x_1 + 1);
-  //       }
-  //     `,
-  //   },
-  //   {
-  //     name: "functions",
-  //     source: `
-  //       function gcd(x, y) = y == 0 ? x : gcd(y, x % y);
-  //       print gcd(22, 33);
-  //     `,
-  //     expected: dedent`
-  //       function gcd_3(x_1, y_2) {
-  //         return (((y_2 == 0)) ? (x_1) : (gcd_3(y_2,(x_1 % y_2))));
-  //       }
-  //       console.log(gcd_3(22,33));
-  //     `,
-  //   },
-  //   {
-  //     name: "standard library",
-  //     source: `
-  //       let x = π;
-  //       print(sin(x) - cos(x) + exp(x) * ln(x) / hypot(2.3, x));
-  //     `,
-  //     expected: dedent`
-  //       let x_1 = Math.PI;
-  //       console.log(((Math.sin(x_1) - Math.cos(x_1)) + ((Math.exp(x_1) * Math.log(x_1)) / Math.hypot(2.3,x_1))));
-  //     `,
-  //   },
+    {
+      name: "while",
+      source: `
+        let x = 0;
+        while x < 5 {
+          let y = 0;
+          while y < 5 {
+            print x * y;
+            y = y + 1;
+          }
+          x = x + 1;
+        }
+      `,
+      expected: dedent`
+        let x_1 = 0;
+        while ((x_1 < 5)) {
+        let y_2 = 0;
+        while ((y_2 < 5)) {
+        console.log((x_1 * y_2));
+        y_2 = (y_2 + 1);
+        }
+        x_1 = (x_1 + 1);
+        }
+      `,
+    },
+    {
+      name: "functions",
+      source: `
+        function gcd(x, y) = y == 0 ? x : gcd(y, x % y);
+        print gcd(22, 33);
+      `,
+      expected: dedent`
+        function gcd_3(x_1, y_2) {
+          return (((y_2 == 0)) ? (x_1) : (gcd_3(y_2,(x_1 % y_2))));
+        }
+        console.log(gcd_3(22,33));
+      `,
+    },
+    {
+      name: "standard library",
+      source: `
+        let x = π;
+        print(sin(x) - cos(x) + exp(x) * ln(x) / hypot(2.3, x));
+      `,
+      expected: dedent`
+        let x_1 = Math.PI;
+        console.log(((Math.sin(x_1) - Math.cos(x_1)) + ((Math.exp(x_1) * Math.log(x_1)) / Math.hypot(2.3,x_1))));
+      `,
+    },
 ]
 
 describe("The code generator", () => {
